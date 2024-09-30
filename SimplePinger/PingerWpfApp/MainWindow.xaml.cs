1b0d900332fb0a0e169add032a2ff3e3841380b3
Kostas Georgiou <>
2024-10-01 00:16:42 +0300

    change: use of simple authenticaiton in wpf

--- SimplePinger/PingerWpfApp/MainWindow.xaml.cs
+++ SimplePinger/PingerWpfApp/MainWindow.xaml.cs
@@ -1,6 +1,15 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
+using Missionware.Cognibase.Client;
+using Missionware.Cognibase.UI.Common.ViewModels;
+using Missionware.Cognibase.UI.Wpf;
 using Missionware.Cognibase.UI.Wpf.Client;
+using Missionware.SharedLib.UI.Wpf.Lib;
+
+using PingerDomain.Entities;
+
+using PingerUiCommon.ViewModels;
 
 namespace PingerApp
 {
@@ -9,11 +18,52 @@ namespace PingerApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WpfStartupHelper _startupHelper;
+        private AsyncWpfDialog _dialog;
+        private readonly MainViewModel _vm;
+
+        public static MainWindow MainWindowFactory()
+        {
+            return new MainWindow();
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _vm = new MainViewModel(App.Client, _dialog);
+            DataContext = _vm;
         }
 
         public static WpfApplication App { get; set; }
+
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            App.Client.Close();
+        }
+
+        protected override void OnContentRendered(EventArgs e)
+        {
+            base.OnContentRendered(e);
+
+            _startupHelper = new WpfStartupHelper(this, App.Client);
+            _startupHelper.AuthVm = new SimpleAuthDialogVm { DomainFullName = "Basic", Username = "user1", Password = "user1" };
+            _startupHelper.QuitAction = () => Close();
+            _startupHelper.DataLoadAction = () =>
+            {
+                // read devices
+                DataItemCollection<Device> collection = App.Client.ReadDataItemCollection<Device>();
+
+                // set data source in main thread
+                Dispatcher.Invoke(() =>
+                {
+                    _vm.Devices = collection;
+                });
+            };
+
+            _startupHelper.ShowAuthDialog();
+        }
     }
 }
