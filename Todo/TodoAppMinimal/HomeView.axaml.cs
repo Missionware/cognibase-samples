64ca8aea1d8f5f3cf04ce3d36db2465dccff8b9d
Kostas Georgiou <>
2024-12-19 20:49:18 +0200

    add: todo minimal

---
+++ Todo/TodoAppMinimal/HomeView.axaml.cs
@@ -0,0 +1,151 @@
+using System;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+using Missionware.Cognibase.Client;
+using Missionware.Cognibase.Library;
+
+using TodoDomain.Entities;
+
+using static Missionware.SharedLib.Serialization.SerializedPropertyBinder;
+
+namespace TodoAppMinimal;
+
+public partial class HomeView : UserControl
+{
+    IClient _client;
+    public HomeView()
+    {
+        InitializeComponent();
+    }
+
+    public void SetupView(IClient client, DataItemCollection<ToDoItem> items)
+    {
+        TodoListBox.ItemsSource = items;
+        _client = client;
+    }
+
+    private async void CheckBox_IsCheckedChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is CheckBox checkBox)
+        {
+            var item = checkBox.DataContext as ToDoItem;
+
+            if (item.IsChanged)
+            {
+                // save
+                var saveResult = await _client.SaveAsync(item);
+
+                // if not successful close form
+                if (!saveResult.WasSuccessful)
+                    _client.ResetAllMonitoredItems();
+                else
+                    NewTodoTextBox.Focus();
+            }
+        }
+    }
+    
+
+    // Handle adding new Todo items
+    private async void NewTodoTextBox_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter && !string.IsNullOrWhiteSpace(NewTodoTextBox.Text))
+        {
+            var item = new ToDoItem { Description = NewTodoTextBox.Text };
+
+            // save
+            var saveResult = await _client.SaveAsync(item);
+
+            // if not successful close form
+            if (!saveResult.WasSuccessful)
+                _client.ResetAllMonitoredItems();
+            else
+                NewTodoTextBox.Text = string.Empty;
+        }
+    }
+
+    private async void ItemTextBox_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (sender is TextBox textBox && e.Key == Key.Enter)
+        {
+            var item = textBox.DataContext as ToDoItem;
+
+            if (item.IsChanged)
+            {
+                // save
+                var saveResult = await _client.SaveAsync(item);
+
+                // if not successful close form
+                if (!saveResult.WasSuccessful)
+                    _client.ResetAllMonitoredItems();
+                else
+                    NewTodoTextBox.Focus();
+            }
+        }
+    }
+
+    // Apply changes when the TextBox loses focus
+    private void ItemTextBox_LostFocus(object? sender, RoutedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            // Exit edit mode
+            textBox.IsReadOnly = true;
+            textBox.Background = Brushes.Transparent; // Revert background
+
+            _client.ResetAllMonitoredItems();
+        }
+    }
+
+    private void ItemTextBox_GotFocus(object? sender, GotFocusEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            // Exit edit mode
+            textBox.CaretBrush = Brushes.Transparent;
+            textBox.BorderBrush = Brushes.Transparent;
+        }
+    }
+
+    private void ItemTextBox_DoubleTapped(object? sender, TappedEventArgs e)
+    {
+        if (sender is TextBox textBox)
+        {
+            // Enter edit mode
+            textBox.IsReadOnly = false;
+            textBox.CaretBrush = Brushes.White;
+            textBox.CaretIndex = textBox.Text.Length; // Move the cursor to the end
+            textBox.SelectionStart = textBox.CaretIndex;
+            textBox.SelectionEnd = textBox.CaretIndex;
+        }
+    }
+
+    private async void DeleteButton_Click(object? sender, RoutedEventArgs e)
+    {
+        if (sender is Button button)
+        {
+            var item = button.DataContext as ToDoItem;
+
+            // check the item is not null
+            if (item != null)
+            {
+                // mark for deletion
+                item.MarkForDeletion();
+
+                // save
+                ClientTransactionInfo saveResult = await _client.SaveAsync(item);
+
+                // if not success unmark (reset edit)
+                if (!saveResult.WasSuccessful)
+                    item.UnMarkForDeletion();
+
+            }
+        }
+       
+    }
+}
